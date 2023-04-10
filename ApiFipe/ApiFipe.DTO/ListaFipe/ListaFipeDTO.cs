15212a48e7a89f38421b5bc85b4caf1ad3ceb540
Ewerton Guimarães <ewerton.guimaraes@becomex.com.br>
2023-04-10 09:34:03 -0300

    Adição de controller e dto simulando consulta no banco (no caso usando classe estática)

---
+++ ApiFipe/ApiFipe.DTO/ListaFipe/ListaFipeDTO.cs
@@ -0,0 +1,43 @@
+﻿using ApiFipe.Domain.Fabricante.Carros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFipe.DTO.ListaFipe
+{
+    public static class ListaFipeDTO
+    {
+
+        public static List<Carro> Carros { get; set; }
+
+        public static List<Carro>  GetListaCarros()
+        {
+            return Carros;
+        }
+
+        public static List<Carro> AddCarro(Carro carro)
+        {
+            Carros.Add(carro);
+            return Carros;
+        }
+
+        public static List<Carro> RemoveCarro(Carro carro)
+        {
+            Carros.Remove(carro);
+            return Carros;
+        }
+
+        public static List<Carro> EditCarro(Carro carro)
+        {
+            var carroOld = Carros.First(x => x.IdCarro == carro.IdCarro);
+            Carros.Remove(carroOld);
+            Carros.Add(carro);
+            return Carros;
+        }
+
+
+    }
+}
