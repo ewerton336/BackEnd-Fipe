1ae85f94d7538f7bb120a8c706ea14c06872b00f
Ewerton Guimarães <ewerton.guimaraes@becomex.com.br>
2023-04-10 09:23:01 -0300

    adição de biblioteca de classe, fabricante e carro para conceito inicial

---
+++ ApiFipe/ApiFipe.Domain/Carro/Carro.cs
@@ -0,0 +1,22 @@
+﻿namespace ApiFipe.Domain.Fabricante.Carros
+{
+    public class Carro
+    {
+        public int CodigoFipe { get; set; }
+        public int IdCarro { get; set; }
+        public int IdModelo { get; set; }
+        public string? NomeCarro { get; set; }
+        public string? DsAnoModelo { get; set; }
+        public string? DsCombustivel { get; set; }
+        public string? DsCodigoFipe { get; set; }
+        public string? DsMesReferencia { get; set; }
+        public string? DsTipoVeiculo { get; set; }
+        public string? DsSiglaCombustivel { get; set; }
+        public decimal? VlValor { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime DtInclusao { get; set; }
+        public DateTime? DtAlteracao { get; set; }
+        public bool? BtAtivo { get; set; }
+        public Fabricante? Fabricante { get; set; }
+    }
+}
---
+++ ApiFipe/ApiFipe.Domain/Fabricante/Fabricante.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiFipe.Domain.Fabricante.Carros
+{
+    public  class Fabricante
+    {
+        public int IdFabricante { get; set; }
+        public string? NomeFabricante { get; set; }
+        public string? DsSigla { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime DtInclusao { get; set; }
+        public DateTime? DtAlteracao { get; set; }
+        public bool? BtAtivo { get; set; }
+    }
+ 
+}
